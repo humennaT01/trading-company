512bb0ed96f79330a264e337daf57a6a3f99cdcc
Tetiana Humenna <tetiana.humenna12@gmail.com>
2020-11-11 16:45:51 +0200

    Added BusinessLogicTests project

---
+++ BusinessLogicTests/AdminManagerTests.cs
@@ -0,0 +1,15 @@
+﻿using BusinessLogic.Concrete;
+using DAL.Interfaces;
+using DTO;
+using Moq;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace BusinessLogicTests
+{
+    [TestFixture]
+    public class AdminManagerTests
+    {
+        
+    }
+}
